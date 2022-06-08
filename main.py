564530ac97e8c753dd1cab1d1df5b3d5d5467be4
dima <dprofe@yandex.ru>
2022-06-09 00:09:44 +0300

    Changes

--- main.py
+++ main.py
@@ -1,27 +1,68 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, make_response
 
-from user import User
+from user import User, SERVER
+
 
 app = Flask(__name__)
-db_name = 'static/users_db.db'
-app.config['current_user'] = None
+# hahaha
+if SERVER:
+    db_data = {
+        'host': 'dprofe.mysql.pythonanywhere-services.com',
+        'user': 'dprofe',
+        'password': '<HIDDEN>',
+        'database': 'dprofe$users',
+    }
+else:
+    db_data = {
+        'database': 'static/users_db.db'
+    }
 app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        app.config['current_user'] = None
-    return render_template('index.html', user=app.config['current_user'], hide_home_link=True)
+    if False:
+        global expire_user
+        if request.method == 'POST':
+            resp = make_response()
+            expire_user = request.cookies.get('user_login')
+            User.remove_from_cookies(resp)
+
+            return resp
+        else:
+            print('NOT NONE')
+
+            print('USER:', user)
+            try:
+                if user.login == expire_user:
+                    expire_user = ''
+                    return redirect('/', 301)
+            except AttributeError:
+                pass
+            resp = make_response(render_template('index.html', user=user, hide_home_link=True))
+            return resp
+
+    user = User.find_by_login(request.cookies.get('user_login'), db_data)
+    return render_template('index.html', user=user, hide_home_link=True)
+
+
+@app.route('/logout')
+def logout():
+    resp = redirect('/', 302)
+    User.remove_from_cookies(resp)
+    return resp
 
 
 @app.route('/save', methods=['POST'])
 def save():
     notes = request.form['notes']
-    if app.config['current_user'] is not None:
-        app.config['current_user'].notes = notes
-        app.config['current_user'].write_to_db(db_name)
-    return redirect('/', 301)
+    login = request.cookies.get('user_login')
+    if login is not None:
+        user = User.find_by_login(login, db_data)
+        user.notes = notes
+        user.write_to_db(db_data)
+
+    return redirect('/', 302)
 
 
 @app.errorhandler(404)
@@ -31,17 +72,20 @@ def err404(e):
 
 @app.route('/auth', methods=['POST', 'GET'])
 def auth():
-    if app.config['current_user'] is not None:
-        return redirect('/', 301)
+    if request.cookies.get('user_login') is not None:
+        return redirect('/', 302)
     if request.method == 'POST':
         login = request.form['login']
         password = request.form['password']
-        user = User.find_by_login(login, db_name)
+        user = User.find_by_login(login, db_data)
         if user is not None:
             if password == user.password:
-                app.config['current_user'] = user
                 flash('Вы успешно вошли', 'success')
-                return render_template('auth.html', redirect_timeout=1000, redirect_address='/')
+
+                resp = make_response(render_template('auth.html', redirect_timeout=1000, redirect_address='/'))
+                User.save_to_cookies(user, resp)
+
+                return resp
             else:
                 flash(f'Неверный пароль для пользователя "{login}"', 'error')
                 return render_template('auth.html', login=login, password=password)
@@ -54,6 +98,8 @@ def auth():
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.cookies.get('user_login') is not None:
+        return redirect('/', 302)
     if request.method == 'POST':
         login = request.form['login']
         password = request.form['password']
@@ -63,21 +109,22 @@ def signup():
             flash(f'Пароли не совпадают', 'error')
             return render_template('singup.html', login=login, password=password)
 
-        elif User.find_by_login(login, db_name) is not None:
+        elif User.find_by_login(login, db_data) is not None:
             flash(f'Пользователь с именем "{login}" уже существует.', 'error')
             return render_template('singup.html', login=login, password=password, password2=password2)
 
         else:
             user = User(login, password, '')
-            user.write_to_db(db_name)
-
-            app.config['current_user'] = user
+            user.write_to_db(db_data)
 
             flash('Вы успешно зарегистрированы', 'success')
-            return render_template('singup.html', redirect_timeout=1000, redirect_address='/')
+
+            resp = make_response(render_template('singup.html', redirect_timeout=1000, redirect_address='/'))
+            User.save_to_cookies(user, resp)
+            return resp
     else:
         return render_template('singup.html')
 
 
 if __name__ == '__main__':
-    app.run('192.168.0.200', 5000, True)
+    app.run('192.168.0.200', 5000, not SERVER)
--- user.py
+++ user.py
@@ -1,4 +1,9 @@
-import sqlite3
+SERVER = False
+
+if SERVER:
+    from mysql import connector
+else:
+    import sqlite3 as connector
 
 
 class User:
@@ -7,10 +12,11 @@ class User:
         self.password = password
         self.notes = notes
 
-    def write_to_db(self, db_name):
-        db_conn = sqlite3.connect(db_name)
+    def write_to_db(self, db_data):
+        User.create_table(db_data)
+        db_conn = connector.connect(**db_data)
         cur = db_conn.cursor()
-        if User.find_by_login(self.login, db_name) is None:
+        if User.find_by_login(self.login, db_data) is None:
             cur.execute(f"insert into users values ('{self.login}', '{self.password}', '{self.notes}')")
         else:
             cur.execute(
@@ -21,8 +27,9 @@ class User:
         db_conn.commit()
 
     @staticmethod
-    def get_list(db_name):
-        db_conn = sqlite3.connect(db_name)
+    def get_list(db_data):
+        User.create_table(db_data)
+        db_conn = connector.connect(**db_data)
         cur = db_conn.cursor()
         cur.execute('SELECT * FROM users')
         res = []
@@ -31,8 +38,35 @@ class User:
         return res
 
     @staticmethod
-    def find_by_login(login: str, db_name):
-        for i in User.get_list(db_name):
+    def find_by_login(login: str, db_data):
+        User.create_table(db_data)
+        for i in User.get_list(db_data):
             if i.login == login:
                 return i
         return None
+
+    @staticmethod
+    def create_table(db_data):
+        db_conn = connector.connect(**db_data)
+        cur = db_conn.cursor()
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                Login NVARCHAR(100) UNIQUE PRIMARY KEY NOT NULL,
+                Password NVARCHAR(100) NOT NULL,
+                Notes NVARCHAR(100)
+            )
+            """
+        )
+
+    @staticmethod
+    def save_to_cookies(user, resp):
+        resp.set_cookie('user_login', user.login, 60 * 60 * 24 * 365 * 100)
+        # on 100 years
+
+    @staticmethod
+    def remove_from_cookies(resp):
+        resp.set_cookie('user_login', '', 0)
+        # on 0 seconds (expire and remove)
+
+
